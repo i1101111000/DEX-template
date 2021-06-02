09c9deb2fd8b6a2ddafc2ce0034c088105566747
i1101111000 <i1101111000@users.noreply.github.com>
2021-06-02 19:05:18 +0300

    erc-20 dex template

---
+++ client/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-center">
+        <a href="https://github.com/i1101111000" style={{ color: 'white' }}></a> REEFOX DECENTRALISED EXCHANGE
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
